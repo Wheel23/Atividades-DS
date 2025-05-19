fac3e513dfc1365d86e35953bc577752efd66402
LUIZ FERNANDO RIBEIRO SANTOS <luiz_santos25@estudante.sesisenai.org.br>
2025-05-19 10:04:54 -0300

    adicionando funções

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dbConnection = require('./db')
+const { criarUsuario } = require('./teste')
+const { setgroups } = require('process')
 
 
 const app = express()
@@ -18,6 +20,54 @@ app.get('./usuarios', async(req,res) => {
     }
 })
 
+app.post('/usuarios', async (req,res) => {
+try{
+    const {nome, email} = req.body
+    await criarUsuario(
+        'INSERT INTO usuarios (nome, email) VALUES (?,?)', 
+        [nome,email]
+    )
+    res.status(201).json({msg: "Criado com success"})
+}catch(err){
+    let msg_erro = "internal Server Error"
+    let cod_erro = 500
+    console.log(err)
+    if(err.erro = 19){
+        msg_erro = "Email já existe"
+        cod_erro = 400
+    }
+    res.status(cod_erro).json({err:msg_erro})
+}
+})
+
+app.patch('/usuarios', async (req,res) => {
+    const userId = parseInt(req.params.id);
+    const user = setup.find(u => u.id === userId);
+  
+    if (!user) {
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+  
+   
+    const { name, email } = req.body;
+    if (name !== undefined) user.name = name;
+    if (email !== undefined) user.email = email;
+  
+    res.json(user);
+})
+
+app.delete('/usuarios', async (req,res) => {
+    const userId = parseInt(req.params.id);
+    const index = setup.findIndex(u => u.id === userId);
+  
+    if (index === -1) {
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+  
+    // Remove o usuário
+    const deletedUser = users.splice(index, 1);
+    res.json({ message: 'Usuário removido com sucesso', user: deletedUser[0] });
+})
 
 dbConnection().then(() => {
 app.listen(8000, () => {console.log("SERVIDOR RODANDO")})
--- teste.js
+++ teste.js
@@ -11,14 +11,14 @@ async function conectar() {
     setup()
 }
 
-function setup(db){
-    db.exec(`CREATE TABLE usuarios (
+async function setup(db){
+    await db.exec(`CREATE TABLE usuarios (
    id INTEGER PRIMARY KEY AUTOINCREMENT, 
    nome TEXT NOT NULL,
    email TEXT NOT NULL UNIQUE
     )`)
 
-    db.exec(`CREATE TABLE IF NOT EXISTs tarefas ( 
+    await db.exec(`CREATE TABLE IF NOT EXISTs tarefas ( 
         id INTEGER PRIMARY KEY AUTOINCREMENT, 
         titulo TEXT NOT NULL,
         descricao TEXT NOT NULL UNIQUE,
